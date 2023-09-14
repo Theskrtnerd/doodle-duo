f9ac7d551212d21952f8be06691c888d2eabc723
Mealsise <Tyler@roe-smith.com>
2023-09-14 10:48:37 +0930

    Update testing.cpp

--- testing.cpp
+++ testing.cpp
@@ -1,7 +1,24 @@
-#include <iostream>
+#include <SFML/Graphics.hpp>
 
-using cout == std::cout;
+int main()
+{
+    // Create a window with dimensions 800x600
+    sf::RenderWindow window(sf::VideoMode(1600, 1000), "SFML Works!");
 
-int main(void) {
+    while (window.isOpen())
+    {
+        // Handle events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // Clear and display window
+        window.clear(sf::Color::Black);
+        window.display();
+    }
+
     return 0;
 }

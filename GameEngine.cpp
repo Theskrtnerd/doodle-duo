57e4d644486c4c31b50100f26a1d73a1903372cb
Theskrtnerd <tvbbd2@gmail.com>
2023-10-05 15:53:29 +1030

    added the json reader

--- GameEngine.cpp
+++ GameEngine.cpp
@@ -1,9 +1,41 @@
 #include "GameEngine.h"
 
-int var = 40;
+// adding dependencies for the json reader
+#include <iostream>
+#include <json/json.h>
+#include <fstream>
+#include <sstream>
+
+// Function to read and parse a JSON file
+Json::Value readJSONFile(const std::string& filePath) {
+    Json::CharReaderBuilder reader;
+    Json::Value root;
+    std::string errors;
+
+    std::ifstream jsonFile(filePath);
+
+    if (!jsonFile.is_open()) {
+        std::cerr << "Error opening JSON file: " << filePath << std::endl;
+        // You can handle this error in your own way, e.g., by returning an empty Json::Value.
+        // Alternatively, you can throw an exception to indicate an error.
+    }
+
+    // Parse the JSON file
+    if (!Json::parseFromStream(reader, jsonFile, &root, &errors)) {
+        std::cerr << "Error parsing JSON file: " << errors << std::endl;
+        // You can handle this error in your own way, e.g., by returning an empty Json::Value.
+        // Alternatively, you can throw an exception to indicate an error.
+    }
+
+    jsonFile.close();
+    return root;
+}
+
+
+int cellSize = 40;
 
 GameEngine::GameEngine(int width, int height, const std::string &title)
-    : window(sf::VideoMode(width, height), title), map(width / var, height / var, var)
+    : window(sf::VideoMode(width, height), title), map(width / cellSize, height / cellSize, cellSize)
 {
     // Set the frame rate (60 frames per second)
     window.setFramerateLimit(60);

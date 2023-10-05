29fe539f074b98e5b10591d47be275578c4f5594
Theskrtnerd <tvbbd2@gmail.com>
2023-10-05 15:49:11 +1030

    added the json reader

--- Map.cpp
+++ Map.cpp
@@ -1,5 +1,36 @@
 #include "Map.h"
 #include <cstdlib> // For generating random values
+#include <iostream>
+
+// adding dependencies for the json reader
+#include <json/json.h>
+#include <fstream>
+#include <sstream>
+
+// Function to read and parse a JSON file
+Json::Value readJSONFile(const std::string& filePath) {
+    Json::CharReaderBuilder reader;
+    Json::Value root;
+    std::string errors;
+
+    std::ifstream jsonFile(filePath);
+
+    if (!jsonFile.is_open()) {
+        std::cerr << "Error opening JSON file: " << filePath << std::endl;
+        // You can handle this error in your own way, e.g., by returning an empty Json::Value.
+        // Alternatively, you can throw an exception to indicate an error.
+    }
+
+    // Parse the JSON file
+    if (!Json::parseFromStream(reader, jsonFile, &root, &errors)) {
+        std::cerr << "Error parsing JSON file: " << errors << std::endl;
+        // You can handle this error in your own way, e.g., by returning an empty Json::Value.
+        // Alternatively, you can throw an exception to indicate an error.
+    }
+
+    jsonFile.close();
+    return root;
+}
 
 Map::Map(int width, int height, int cellSize) : width_(width), height_(height), cellSize_(cellSize)
 {

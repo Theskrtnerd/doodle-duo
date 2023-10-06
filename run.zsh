b4b20ef5ecd1095895eb220ec9625fb337e9b8a6
Mealsise <Tyler@roe-smith.com>
2023-10-06 16:04:33 +1030

    Update run.zsh

--- run.zsh
+++ run.zsh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Compile the C++ code
-g++ -o doodle_duo gametest.cpp Map.cpp GameObject.cpp ImmoveableObject.cpp GameEngine.cpp -lsfml-graphics -lsfml-window -lsfml-system -ljsoncpp
+#//g++ -o doodle_duo gametest.cpp Map.cpp GameObject.cpp ImmoveableObject.cpp GameEngine.cpp -lsfml-graphics -lsfml-window -lsfml-system -ljsoncpp
+g++ -Wpedantic -o doodle_duo gametest.cpp Map.cpp GameObject.cpp ImmoveableObject.cpp GameEngine.cpp -lsfml-graphics -lsfml-window -lsfml-system -ljsoncpp -I/usr/include/jsoncpp
+
 
 # Check if compilation was successful
 if [ $? -eq 0 ]; then

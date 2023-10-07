970c75dfecdeca128053465b1c917855fc897e8c
Mealsise <Tyler@roe-smith.com>
2023-10-07 10:36:14 +1030

    Update run.zsh

--- run.zsh
+++ run.zsh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-# Compile the C++ code
-#//g++ -o doodle_duo gametest.cpp Map.cpp GameObject.cpp ImmoveableObject.cpp GameEngine.cpp -lsfml-graphics -lsfml-window -lsfml-system -ljsoncpp
-g++ -Wpedantic -o doodle_duo gametest.cpp Map.cpp GameObject.cpp GameObjectArray.cpp MoveableObject.cpp ImmoveableObject.cpp GameEngine.cpp Player.cpp functions.cpp -lsfml-graphics -lsfml-window -lsfml-system -ljsoncpp -I/usr/include/jsoncpp
+# List of source files
+source_files=(
+    gametest.cpp 
+    Map.cpp 
+    GameObject.cpp 
+    GameObjectArray.cpp 
+    MoveableObject.cpp 
+    ImmoveableObject.cpp 
+    GameEngine.cpp 
+    Player.cpp 
+    functions.cpp
+)
 
+# Compile the C++ code
+g++ -Wpedantic -o doodle_duo "${source_files[@]}" -lsfml-graphics -lsfml-window -lsfml-system -ljsoncpp -I/usr/include/jsoncpp
+#//g++ -Wpedantic -o doodle_duo gametest.cpp Map.cpp GameObject.cpp GameObjectArray.cpp MoveableObject.cpp ImmoveableObject.cpp GameEngine.cpp Player.cpp functions.cpp -lsfml-graphics -lsfml-window -lsfml-system -ljsoncpp -I/usr/include/jsoncpp
 
 # Check if compilation was successful
 if [ $? -eq 0 ]; then

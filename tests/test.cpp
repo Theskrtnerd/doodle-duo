e2637583582517e2923f802e0cc53d652e3913ee
Theskrtnerd <tvbbd2@gmail.com>
2023-10-17 11:11:19 +1030

    add unit test

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,5 +1,21 @@
 #define CATCH_CONFIG_MAIN
 #include<catch2/catch.hpp>
 
+#include "include/MoveableObject.h"
+#include "include/Cell.h"
+#include "include/Obstacle.h"
+#include "include/GameObject.h"
+#include "include/Background.h"
+#include "include/ScreenButton.h"
+#include "include/Button.h"
+#include "include/functions.h"
+#include "include/Lever.h"
+#include "include/Door.h"
+#include "include/GameObjectArray.h"
+#include "include/GameTextures.h"
+#include "include/GameEngine.h"
+#include "include/Player.h"
+#include "include/Exit.h"
+#include "include/ImmoveableObject.h"
+#include "include/Text.h"
 
-#include "class/"
